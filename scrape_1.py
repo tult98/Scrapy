b58fe055f5084d89ad546d0d82f072aac90e7a84
lethanhtupk <lethanhtu1551998@gmail.com>
2019-11-23 23:01:11 +0700

    crawler dynamic website

--- scrape_1.py
+++ scrape_1.py
@@ -1,42 +1,76 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import requests
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+import requests
+import time
 import csv
 
-# csv_file = open('data.csv', 'w')
-# csv_writer = csv.writer(csv_file)
-# csv_writer.writerow(['image_url'])
-
+csv_file = open('clean_data.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['image_url'])
 
 # access the browser driver 
 chrome_options = Options()
-chrome_options.headless = True
-driver = webdriver.Chrome(
-    executable_path="./chromedriver.exe", options=chrome_options)
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(ChromeDriverManager().install(),options = chrome_options)
 
-# scroll the webpage 
-driver.get(
-    "https://vwin99.net/mrcong-mu-fei-fei-anh-nung-gai-xinh-them-du-la-day-chu-dau/")
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-page_source = driver.page_source
+count = 0
 
- 
-soup = BeautifulSoup(page_source, 'lxml')
-# print(soup.prettify())
-# get the image_url
-entry_content = soup.find('div', class_="entry-content")
-# print(entry_content.prettify())
+# get the number of page in website 
+source = requests.get("https://vwin99.net/anh-hot-girl-18/").text
+soup = BeautifulSoup(source, 'lxml')
+nav_bar = soup.find('div', class_="nav-links")
+nav_bar_link  = nav_bar.find_all('a')
+page_number = nav_bar_link[-2].text
 
-images_content = entry_content.find_all('p')
+for i in range(int(page_number)):
+    source = requests.get(f"https://vwin99.net/anh-hot-girl-18/page/{i+1}/").text
+    soup = BeautifulSoup(source, 'lxml')
+    content_page = soup.find("main", class_="site-main")
+    articles = content_page.find_all('div', class_="inside-article")
+    for article in articles:
+        post_link = article.header.h2.a['href']
+        # scroll to the bottom of the webpage
+        # solution from here: https://stackoverflow.com/questions/51836275/finding-the-number-of-pages-on-a-website-with-soup-findall-unicode-problems 
+        driver.get(post_link)
+        SCROLL_PAUSE_TIME = 0.5
 
-for image_content in images_content:
-    print(image_content)
-    print('\n')
+        # Get scroll height
+        last_height = driver.execute_script("return document.body.scrollHeight")
 
+        while True:
+            # Scroll down to bottom
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # Wait to load page
+            time.sleep(SCROLL_PAUSE_TIME)
+
+            # Calculate new scroll height and compare with last scroll height
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+        page_source = driver.page_source
+
+        # get the image url
+        soup = BeautifulSoup(page_source, 'lxml')
+        entry_content = soup.find('div', class_="entry-content")
+        images_content = entry_content.find_all('p')
+        for image_content in images_content:
+            images = image_content.find_all("img", class_="lazy")
+            if len(images) > 0:
+                for image in images: 
+                    image_url = image['src']
+                    if "https://" in image_url:
+                        count += 1
+                        csv_writer.writerow([image_url])
+                        print(image_url)
+
+csv_file.close()
+print(count)
 driver.quit()
-# print(entry_content.prettify())
+
 
 
 
